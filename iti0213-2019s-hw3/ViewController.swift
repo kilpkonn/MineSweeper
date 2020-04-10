a1383b88bd00b6284ba78cddca25440e9496b75e
taannu <taannu@ttu.ee>
2020-04-10 08:31:35 +0200

    Initial Commit

---
+++ iti0213-2019s-hw3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iti0213-2019s-hw3
+//
+//  Created by user169473 on 4/10/20.
+//  Copyright © 2020 taannu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
