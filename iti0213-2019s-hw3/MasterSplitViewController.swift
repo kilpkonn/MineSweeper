e472ba54f5b988e3343d79986d247ea8377fb3f4
taannu <taannu@ttu.ee>
2020-04-20 09:22:13 +0200

    How does it work?

---
+++ iti0213-2019s-hw3/MasterSplitViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MasterSplitViewController.swift
+//  iti0213-2019s-hw3
+//
+//  Created by user169473 on 4/20/20.
+//  Copyright © 2020 taannu. All rights reserved.
+//
+
+import UIKit
+
+class MasterSplitViewController: UISplitViewController, UISplitViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.delegate = self
+        self.preferredDisplayMode = .allVisible
+        
+    }
+    
+
+    func splitViewController(_ svc: UISplitViewController, collapseSecondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        return true
+    }
+
+}
