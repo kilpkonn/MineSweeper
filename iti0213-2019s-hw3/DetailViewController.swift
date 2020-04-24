810136255f9880804448f11add09e5d1104227a7
taannu <taannu@ttu.ee>
2020-04-24 10:48:23 +0200

    Somewhat working split view

---
+++ iti0213-2019s-hw3/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  iti0213-2019s-hw3
+//
+//  Created by user169473 on 4/10/20.
+//  Copyright © 2020 taannu. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+   
+}
+
